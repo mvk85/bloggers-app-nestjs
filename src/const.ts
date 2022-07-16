6b6e2b0a6c18fe12e7183f9569815d71036498d6
mvk85 <messages.mvk@gmail.com>
2022-07-16 20:55:50 +0300

    was created common const

---
+++ src/const.ts
@@ -0,0 +1,10 @@
+export const DEFAULT_PAGE_NUMBER = 1;
+export const DEFAULT_PAGE_SIZE = 10;
+
+export const removeObjectIdOption = { _id: 0, __v: 0 };
+
+export const commentsProjection = { ...removeObjectIdOption, postId: 0 };
+
+export const projectionUserItem = { _id: 0, id: 1, login: 1, email: 1 };
+
+export const projectionCreateUserItem = { _id: 0, id: 1, login: 1 };
