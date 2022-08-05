c651dab4ffcf35a408100e6d81cfa5e8292a9dfb
mvk85 <messages.mvk@gmail.com>
2022-08-05 21:29:59 +0300

    fixed post schema

--- src/db/schemas.mongoose.ts
+++ src/db/schemas.mongoose.ts
@@ -30,6 +30,7 @@ export const postsSchema = new mongoose.Schema<PostDbEntity>({
   content: { type: String, required: true },
   bloggerId: { type: String, required: true },
   bloggerName: { type: String, required: true },
+  addedAt: { type: Date, requered: true },
   // TODO maybe use sub-schema will be better for likes?
   likes: {
     status: {
