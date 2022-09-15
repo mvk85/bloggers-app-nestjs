38ddae7910bd814ab3cfa186b754ec4badb73a47
mvk85 <messages.mvk@gmail.com>
2022-09-15 18:41:48 +0300

    posts e2e tests was added

--- test/features/posts/posts.e2e-spec.ts
+++ test/features/posts/posts.e2e-spec.ts
@@ -12,6 +12,9 @@ import { DEFAULT_PAGE_NUMBER, DEFAULT_PAGE_SIZE } from 'src/const';
 import { TestComments } from '../comments/helpers/TestComments';
 import { HelperComment } from '../comments/helpers/HelperComments';
 import { CommonTestHelper } from 'test/helper/CommonTestHelper';
+import { PostCreateFields } from 'src/feature/posts/types';
+import { TestUsers } from '../users/helpers/TestUsers';
+import { LikesStatus } from 'src/db/types';
 
 jest.setTimeout(1000 * 100);
 
@@ -24,6 +27,7 @@ describe('posts api e2e tests', () => {
   let testComments: TestComments;
   let helperComment: HelperComment;
   let commonTestHelper: CommonTestHelper;
+  let testUsers: TestUsers;
 
   beforeAll(async () => {
     const testingModule: TestingModule = await Test.createTestingModule({
@@ -40,6 +44,7 @@ describe('posts api e2e tests', () => {
     authHelper = new AuthHelper(testingModule);
     testComments = new TestComments(testingModule);
     helperComment = new HelperComment(testingModule);
+    testUsers = new TestUsers(testingModule);
     commonTestHelper = new CommonTestHelper();
   });
 
@@ -48,12 +53,11 @@ describe('posts api e2e tests', () => {
     urlBuilder.clear();
   };
 
-  beforeEach(async () => {
-    await clear();
-  });
-
   describe('posts api', () => {
-    describe('/post', () => {
+    describe('/post GET', () => {
+      beforeEach(async () => {
+        await clear();
+      });
       it('should return posts without pagination params', async () => {
         const { post: post1 } = await testPosts.make();
         const { post: post2 } = await testPosts.make();
@@ -79,7 +83,11 @@ describe('posts api e2e tests', () => {
         helperPosts.expectPostSchema(response.body.items[1]);
         helperPosts.expectPostSchema(response.body.items[2]);
       });
-
+    });
+    describe('/post POST', () => {
+      beforeEach(async () => {
+        await clear();
+      });
       it('Should create new post when the data is correct', async () => {
         const postCreateFields = await testPosts.makeCreatedObject();
         const apiUrl = urlBuilder.addSubdirectory('posts').build();
@@ -134,7 +142,10 @@ describe('posts api e2e tests', () => {
         helperPosts.expectPostSchema(responsePostCreated.body);
       });
     });
-    describe('/post/:id', () => {
+    describe('/post/:id GET', () => {
+      beforeEach(async () => {
+        await clear();
+      });
       it('should return post by id when post exist (without likes)', async () => {
         const { post } = await testPosts.make();
         const apiUrl = urlBuilder
@@ -179,7 +190,142 @@ describe('posts api e2e tests', () => {
       });
     });
 
+    describe('/post/:id PUT', () => {
+      beforeEach(async () => {
+        await clear();
+      });
+      it('Should update post when fields are correct and exist basic auth', async () => {
+        const { post } = await testPosts.make();
+        const updateObject: PostCreateFields = {
+          bloggerId: post.bloggerId,
+          content: testPosts.generateContent(),
+          title: testPosts.generateTitle(),
+          shortDescription: testPosts.generateShortDescription(),
+        };
+        const apiUrlUpdating = urlBuilder
+          .addSubdirectory('posts')
+          .addSubdirectory(post.id)
+          .build();
+        urlBuilder.clear();
+        const apiUrlReceiving = urlBuilder
+          .addSubdirectory('posts')
+          .addSubdirectory(post.id)
+          .build();
+
+        const responseUpdating = await request(app.getHttpServer())
+          .put(apiUrlUpdating)
+          .set(authHelper.makeBasicHeader())
+          .send(updateObject);
+
+        const responseReceiving = await request(app.getHttpServer()).get(
+          apiUrlReceiving,
+        );
+
+        expect(responseUpdating.status).toEqual(HttpStatus.NO_CONTENT);
+        expect(responseReceiving.status).toEqual(HttpStatus.OK);
+        expect(responseReceiving.body.content).toEqual(updateObject.content);
+        expect(responseReceiving.body.title).toEqual(updateObject.title);
+        expect(responseReceiving.body.shortDescription).toEqual(
+          updateObject.shortDescription,
+        );
+        helperPosts.expectPostSchema(responseReceiving.body);
+      });
+
+      it('Should return 400 when update field is not correct', async () => {
+        const { post } = await testPosts.make();
+        const updateObject: PostCreateFields = {
+          bloggerId: post.bloggerId,
+          content: ' ',
+          title: ' ',
+          shortDescription: ' ',
+        };
+        const apiUrl = urlBuilder
+          .addSubdirectory('posts')
+          .addSubdirectory(post.id)
+          .build();
+
+        const response = await request(app.getHttpServer())
+          .put(apiUrl)
+          .set(authHelper.makeBasicHeader())
+          .send(updateObject);
+
+        const errors = response.body.errorsMessages;
+
+        expect(response.status).toEqual(HttpStatus.BAD_REQUEST);
+        expect(errors.length).toBe(3);
+        commonTestHelper.checkErrors(errors);
+      });
+
+      it('Should return 401 when does not have a basic auth', async () => {
+        const { post } = await testPosts.make();
+        const apiUrl = urlBuilder
+          .addSubdirectory('posts')
+          .addSubdirectory(post.id)
+          .build();
+
+        const response = await request(app.getHttpServer())
+          .put(apiUrl)
+          .send({});
+
+        expect(response.status).toEqual(HttpStatus.UNAUTHORIZED);
+      });
+
+      it('Should return 404 when does not exist postId', async () => {
+        const apiUrl = urlBuilder
+          .addSubdirectory('posts')
+          .addSubdirectory(commonTestHelper.generateRandomUuid())
+          .build();
+
+        const response = await request(app.getHttpServer())
+          .put(apiUrl)
+          .set(authHelper.makeBasicHeader())
+          .send({});
+
+        expect(response.status).toEqual(HttpStatus.NOT_FOUND);
+      });
+    });
+
+    describe('/post/:id DELETE', () => {
+      beforeEach(async () => {
+        await clear();
+      });
+      it('Should delete post when postId exist and have basic auth', async () => {
+        const { post } = await testPosts.make();
+        const apiUrl = urlBuilder
+          .addSubdirectory('posts')
+          .addSubdirectory(post.id)
+          .build();
+
+        const responseToDelete = await request(app.getHttpServer())
+          .delete(apiUrl)
+          .set(authHelper.makeBasicHeader());
+
+        const responseToReceive = await request(app.getHttpServer()).get(
+          apiUrl,
+        );
+
+        expect(responseToDelete.status).toEqual(HttpStatus.NO_CONTENT);
+        expect(responseToReceive.status).toEqual(HttpStatus.NOT_FOUND);
+      });
+
+      it('Should return 404 when postId does not exist', async () => {
+        const apiUrl = urlBuilder
+          .addSubdirectory('posts')
+          .addSubdirectory(commonTestHelper.generateRandomUuid())
+          .build();
+
+        const responseToDelete = await request(app.getHttpServer())
+          .delete(apiUrl)
+          .set(authHelper.makeBasicHeader());
+
+        expect(responseToDelete.status).toEqual(HttpStatus.NOT_FOUND);
+      });
+    });
+
     describe('/post/:id/comments POST', () => {
+      beforeEach(async () => {
+        await clear();
+      });
       it('Should create comment by postId when fields is correct', async () => {
         const { createObject, post, user } =
           await testComments.makeCreatedObject();
@@ -255,6 +401,9 @@ describe('posts api e2e tests', () => {
     });
 
     describe('/post/:id/comments GET', () => {
+      beforeEach(async () => {
+        await clear();
+      });
       it('Should return list of comments by postId without pagination params', async () => {
         const { comment, post } = await testComments.make();
         await testComments.make(); // make comment with another postId
@@ -289,6 +438,127 @@ describe('posts api e2e tests', () => {
         const response = await request(app.getHttpServer()).get(apiUrl);
 
         expect(response.status).toEqual(HttpStatus.NOT_FOUND);
+      });
+    });
+
+    describe('/post/:id/like-status PUT', () => {
+      let userForLike;
+      let postForLike;
+
+      beforeAll(async () => {
+        userForLike = await testUsers.make();
+        const { post } = await testPosts.make();
+        postForLike = post;
+      });
+
+      afterAll(async () => {
+        await clear();
+      });
+
+      beforeEach(() => {
+        urlBuilder.clear();
+      });
+
+      it('Should create like of post when have auth', async () => {
+        const requestBody = { likeStatus: LikesStatus.Like };
+        const apiUrl = urlBuilder
+          .addSubdirectory('posts')
+          .addSubdirectory(postForLike.id)
+          .addSubdirectory('like-status')
+          .build();
+
+        urlBuilder.clear();
+
+        const apiUrlReceiving = urlBuilder
+          .addSubdirectory('posts')
+          .addSubdirectory(postForLike.id)
+          .build();
+
+        const responseToLike = await request(app.getHttpServer())
+          .put(apiUrl)
+          .set(authHelper.makeAccessHeader(userForLike.id))
+          .send(requestBody);
+
+        const responseToReceive = await request(app.getHttpServer()).get(
+          apiUrlReceiving,
+        );
+
+        expect(responseToLike.status).toEqual(HttpStatus.NO_CONTENT);
+        expect(responseToReceive.status).toEqual(HttpStatus.OK);
+        expect(responseToReceive.body.extendedLikesInfo.likesCount).toBe(1);
+        expect(responseToReceive.body.extendedLikesInfo.dislikesCount).toBe(0);
+        expect(
+          responseToReceive.body.extendedLikesInfo.newestLikes.length,
+        ).toBe(1);
+        expect(
+          responseToReceive.body.extendedLikesInfo.newestLikes[0].userId,
+        ).toBe(userForLike.id);
+      });
+
+      it('Should change status of post like when have auth', async () => {
+        const requestBody = { likeStatus: LikesStatus.Dislike };
+        const apiUrl = urlBuilder
+          .addSubdirectory('posts')
+          .addSubdirectory(postForLike.id)
+          .addSubdirectory('like-status')
+          .build();
+
+        urlBuilder.clear();
+
+        const apiUrlToReceive = urlBuilder
+          .addSubdirectory('posts')
+          .addSubdirectory(postForLike.id)
+          .build();
+
+        const responseToLike = await request(app.getHttpServer())
+          .put(apiUrl)
+          .set(authHelper.makeAccessHeader(userForLike.id))
+          .send(requestBody);
+
+        const responseToReceive = await request(app.getHttpServer()).get(
+          apiUrlToReceive,
+        );
+
+        expect(responseToLike.status).toEqual(HttpStatus.NO_CONTENT);
+        expect(responseToReceive.status).toEqual(HttpStatus.OK);
+        expect(responseToReceive.body.extendedLikesInfo.likesCount).toBe(0);
+        expect(responseToReceive.body.extendedLikesInfo.dislikesCount).toBe(1);
+        expect(
+          responseToReceive.body.extendedLikesInfo.newestLikes.length,
+        ).toBe(0);
+      });
+
+      it('Should delete like entity of post when have auth', async () => {
+        const requestBody = { likeStatus: LikesStatus.None };
+        const apiUrl = urlBuilder
+          .addSubdirectory('posts')
+          .addSubdirectory(postForLike.id)
+          .addSubdirectory('like-status')
+          .build();
+
+        urlBuilder.clear();
+
+        const apiUrlToReceive = urlBuilder
+          .addSubdirectory('posts')
+          .addSubdirectory(postForLike.id)
+          .build();
+
+        const responseToLike = await request(app.getHttpServer())
+          .put(apiUrl)
+          .set(authHelper.makeAccessHeader(userForLike.id))
+          .send(requestBody);
+
+        const responseToReceive = await request(app.getHttpServer()).get(
+          apiUrlToReceive,
+        );
+
+        expect(responseToLike.status).toEqual(HttpStatus.NO_CONTENT);
+        expect(responseToReceive.status).toEqual(HttpStatus.OK);
+        expect(responseToReceive.body.extendedLikesInfo.likesCount).toBe(0);
+        expect(responseToReceive.body.extendedLikesInfo.dislikesCount).toBe(0);
+        expect(
+          responseToReceive.body.extendedLikesInfo.newestLikes.length,
+        ).toBe(0);
       });
     });
   });
